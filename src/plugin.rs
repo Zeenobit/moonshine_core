49ee620253f31593a5d61d8116f714e6773f4d4b
Zeenobit <zeenobit.exe@gmail.com>
2025-07-16 12:28:23 -0400

    Update submodules

--- src/plugin.rs
+++ src/plugin.rs
@@ -5,9 +5,6 @@ pub struct MoonshineCorePlugins;
 
 impl PluginGroup for MoonshineCorePlugins {
     fn build(self) -> PluginGroupBuilder {
-        PluginGroupBuilder::start::<Self>()
-            .add(crate::save::SavePlugin)
-            .add(crate::load::LoadPlugin)
-            .add(crate::tag::TagPlugin)
+        PluginGroupBuilder::start::<Self>().add(crate::tag::TagPlugin)
     }
 }
